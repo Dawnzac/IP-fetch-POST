8fbef61dc91d79f7342574390722d3431d580474
Dawn Zac <dawnzac88@gmail.com>
2022-10-23 21:23:55 +0530

    update

--- upload.py
+++ upload.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
+""" #!/usr/bin/env python3
 
 import requests
 
 response = requests.post("http://peas.pushpagiri.in:8085/ip/data_entry.php",
-            files = {"ip_addre":"10.0.0.0"})
+           data = {"ip_addre":"10.0.0.0","name":"testing","location":"Unallocated","floor":"Unallocated"})
 
-print(response.text)
+print(response.text) """
+
+import requests
+url = "http://peas.pushpagiri.in:8085/ip/addnew_ip.php"
+payload = {'ip_addre': '0005989', 'name': 'testing', 'location': 'Unallocated', 'floor': 'Unallocated'}
+with requests.session() as s:
+    # fetch the login page
+    s.get(url)
+
+    # post to the login form
+    r = s.post(url, data=payload)
+    print(r.text)
+
+
+""" import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "http://peas.pushpagiri.in:8085/ip/addnew_ip.php"
+
+session = requests.Session()
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+data = "ip_addre=10.0.0.0&name=testing&location=Unallocated&floor=Unallocated"
+
+
+resp = session.post(url, headers=headers, data=data)
+
+print(resp.status_code) """
+
